e4b52df37e8b3af361cfb8f3113c688a0e686462
Kailash Singaravelu <kailash98s@gmail.com>
2021-11-26 17:54:17 -0500

    Added Roles and channels and handled minor issues

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import json
 import os
 import discord
+from discord.utils import get
 from discord import Intents
 from dotenv import load_dotenv
 from discord.ext.commands import Bot, has_permissions
@@ -38,14 +39,16 @@ bot = Bot(intents=intents, command_prefix="$")
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
+    if guild:
+        print(
+            f"{bot.user} is connected to the following guild:\n"
+            f"{guild.name}(id: {guild.id})"
+            )
 
-    print(
-        f"{bot.user} is connected to the following guild:\n"
-        f"{guild.name}(id: {guild.id})"
-    )
-
-    members = "\n -".join([member.name for member in guild.members])
-    print(f"Guild Members:\n - {members}")
+        members = "\n -".join([member.name for member in guild.members])
+        print(f"Guild Members:\n - {members}")
+    else:
+        print(f"{bot.user} not part of any guild yet.")
 
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
@@ -58,6 +61,79 @@ async def on_ready():
         )
     )
     print("READY!")
+
+# ------------------------------------------------------------------------------------------------------------------
+#    Function: on_guild_join
+#    Description: run when a the bot joins a guild
+#    Inputs:
+#    - guild: the guild the user joined from
+#    Outputs:
+#    -
+# ------------------------------------------------------------------------------------------------------------------
+@bot.event
+async def on_guild_join(guild):
+    channel = get(guild.text_channels, name='general')
+    if channel is None:
+        await guild.create_text_channel('general')
+        channel = get(guild.text_channels, name='general')
+    if channel.permissions_for(guild.me).send_messages:
+        await channel.send('Hi there, I\'m ClassMate Bot, and I\'m here' +
+            'to help you manage your class discord! Let\'s do some quick setup.')
+    role = get(guild.roles, name='Instructor')
+    await guild.create_role(name="Instructor", colour=discord.Colour(0xdc143c),
+                                    permissions=discord.Permissions.all())
+    leadrole = get(guild.roles, name='Instructor')
+    #Assign Instructor role to admin is there is no Instructor
+    if role is None or len(role.members) == 0:
+        leader = guild.owner
+        leadrole = get(guild.roles, name='Instructor')
+        await channel.send(leader.name + " has been given Instructor role!")
+        await leader.add_roles(leadrole, reason=None, atomic=True)
+    else:
+        for member in role.members:
+            await member.add_roles(leadrole, reason=None, atomic=True)
+        instructors = ", ".join([str(x).rsplit("#", 1)[0] for x in leadrole.members])
+        if len(leadrole.members) == 1:
+            await channel.send(instructors + " is the Instructor!")
+        else:
+            await channel.send(instructors + " are the Instructors!")
+    #await channel.send("To add Instructors, type \"!setInstructor @<member>\"")
+    #await channel.send("To remove instructors, type \"!removeInstructor @<member>\"")
+    await guild.create_role(name="TA", colour=discord.Colour(0x00ffff),
+                                    permissions=discord.Permissions.all())
+    await guild.create_role(name="student", colour=discord.Colour(0x7289da),
+                                    permissions=discord.Permissions.all())
+    await guild.create_role(name="guest", colour=discord.Colour(0xfffacd),
+                                    permissions=discord.Permissions.all())
+    #Create Text channels if they don't exist
+    ic_overwrites = {guild.default_role: discord.PermissionOverwrite(read_messages=False,
+        send_messages=False), leadrole: discord.PermissionOverwrite(read_messages=True,
+        send_messages=True)}
+    role = get(guild.roles, name='TA')
+    tc_overwrites = {guild.default_role: discord.PermissionOverwrite(read_messages=False,
+        send_messages=False), leadrole: discord.PermissionOverwrite(read_messages=True,
+        send_messages=True), role: discord.PermissionOverwrite(read_messages=True,
+        send_messages=True)}
+    if get(guild.text_channels, name='instructor_channel') is None:
+        await guild.create_text_channel('instructor_channel', overwrites=ic_overwrites)
+        await channel.send("instructor_channel channel has been added!")
+    else:
+        await channel.send("instructor_channel channel is already present!")
+    if get(guild.text_channels, name='TA_channel') is None:
+        await guild.create_text_channel('TA_channel', overwrites=tc_overwrites)
+        await channel.send("TA_channel channel has been added!")
+    else:
+        await channel.send("TA_channel channel is already present!")
+    if get(guild.text_channels, name='q-and-a') is None:
+        await guild.create_text_channel('q-and-a')
+        await channel.send("q-and-a channel has been added!")
+    else:
+        await channel.send("q-and-a channel is already present!")
+    if get(guild.text_channels, name='course-calendar') is None:
+        await guild.create_text_channel('course-calendar')
+        await channel.send("course-calendar channel has been added!")
+    else:
+        await channel.send("course-calendar channel is already present!")
 
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -77,14 +153,14 @@ async def on_raw_reaction_add(payload):
             email.send_email(email_list[str(payload.user_id)], attachment, filename=filename)
 
 
-# ------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 #    Function: on_member_join(member)
 #    Description: Handles on_member_join events, DMs the user and asks for verification through newComer.py
 #    Inputs:
 #    - member: used to add member to the knowledge of the bot
 #    Outputs:
 #    -
-# ------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 @bot.event
 async def on_member_join(member):
     unverified = discord.utils.get(
@@ -96,7 +172,7 @@ async def on_member_join(member):
         "Verify yourself before getting started! \n To use the verify command, do: $verify <your_full_name> \n \
         ( For example: $verify Jane Doe )")
 
-
+# ------------------------------------------------------------------------------------------------------------------
 #    Function: on_error(event, *args, **kwargs)
 #    Description: Handles bot errors, prints errors to a log file
 #    Inputs:
@@ -105,7 +181,7 @@ async def on_member_join(member):
 #    - **kwargs: other args
 #    Outputs:
 #    -
-# ------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 @bot.event
 async def on_error(event, *args, **kwargs):
     with open("err.log", "a") as f:
@@ -115,14 +191,14 @@ async def on_error(event, *args, **kwargs):
             raise
 
 
-# ----------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 #    Function: on_member_join(member)
 #    Description: Command for shutting down the bot
 #    Inputs:
 #    - ctx: used to access the values passed through the current context
 #    Outputs:
 #    -
-# ----------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 @bot.command(name="shutdown", help="Shuts down the bot, only usable by the owner")
 @has_permissions(administrator=True)
 async def shutdown(ctx):
