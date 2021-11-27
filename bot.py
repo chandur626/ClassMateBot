cfdae8b5b2180a4b26186403a2df962061563187
Kailash Singaravelu <kailash98s@gmail.com>
2021-11-26 22:33:16 -0500

    Added new functionality based on role

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from discord import Intents
 from dotenv import load_dotenv
 from discord.ext.commands import Bot, has_permissions
 from Utility.email_utility import EmailUtility
-import asyncio
+
 # ----------------------------------------------------------------------------------------------
 # Initializes the discord bot with a unique TOKEN and joins the bot to a server provided by the
 # GUILD token. Handles bot shutdown and error events
@@ -25,7 +25,7 @@ UNVERIFIED_ROLE_NAME = os.getenv("UNVERIFIED_ROLE_NAME")
 intents = Intents.all()
 # Set all bot commands to begin with $
 bot = Bot(intents=intents, command_prefix="$")
-spam_log=[]
+
 
 # ------------------------------------------------------------------------------------------------------------------
 #    Function: on_ready()
@@ -61,29 +61,13 @@ async def on_ready():
         )
     )
     print("READY!")
-    while True:
-        print('spam cleared')
-        await asyncio.sleep(10)
-        spam_log.clear()
 
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
-    counter=1    
-    for i in range(len(spam_log)): 
-        if spam_log[i]==str(message.author.id):
-            counter=counter+1
-    spam_log.append(str(message.author.id))
-    if counter>4:
-        await message.channel.send('please stop spamming! '+message.author.mention+', this may lead to a BAN/KICK!')
-        def check(m):
-            return str(m.author) == str(message.author)
-        await message.channel.purge(limit=6, check=check)
+
 # ------------------------------------------------------------------------------------------------------------------
 #    Function: on_guild_join
 #    Description: run when a the bot joins a guild
 #    Inputs:
-#    - guild: the guild the user joined from
+#    - guild: the guild the BOT joins
 #    Outputs:
 #    -
 # ------------------------------------------------------------------------------------------------------------------
@@ -152,6 +136,7 @@ async def on_guild_join(guild):
     else:
         await channel.send("course-calendar channel is already present!")
 
+
 @bot.event
 async def on_raw_reaction_add(payload):
     print(payload)
@@ -206,102 +191,10 @@ async def on_error(event, *args, **kwargs):
             f.write(f"Unhandled message: {args[0]}\n")
         else:
             raise
-# ----------------------------------
-#    Function: load
-#    Description: Command for loading a cog
-#    Inputs:
-#    - ctx: used to access the values passed through the current context
-#    - extension: name of the cog that is to be added
-#    Outputs:
-#    - response from bot
-# ----------------------------------
-@bot.command(name='loadCog',help="loads a specific cog")
-@has_permissions(administrator=True)
-async def load(ctx,extension):
-    bot.load_extension(f"cogs.{extension}")
-    await ctx.send(str(extension)+' cog has been added')
 
-# ----------------------------------
-#    Function: unload
-#    Description: Command for unloading a cog
-#    Inputs:
-#    - ctx: used to access the values passed through the current context
-#    - extension: name of the cog that is to be removed
-#    Outputs:
-#    - response from bot
-# ----------------------------------
-
-@bot.command(name='unloadCog',help="unloads a specific cog")
-@has_permissions(administrator=True)
-async def unload(ctx,extension):
-    bot.unload_extension(f"cogs.{extension}")
-    await ctx.send(str(extension)+' cog has been removed')
-
-
-# ----------------------------------
-#    Function: reload
-#    Description: Command for reloading a cog after an update is made
-#    Inputs:
-#    - ctx: used to access the values passed through the current context
-#    - extension: name of the cog that is to be reloaded
-#    Outputs:
-#    - response from bot
-# ----------------------------------
-
-@bot.command(name='reloadCog',help="reloads a specific cog")
-@has_permissions(administrator=True)
-async def reload(ctx,extension):
-    bot.unload_extension(f"cogs.{extension}")
-    bot.load_extension(f"cogs.{extension}")
-
-    await ctx.send(str(extension)+' cog has been reloaded')
-
-# ----------------------------------
-#    Function: Ban
-#    Description: Command for banning spammers
-#    Inputs:
-#    - ctx: used to access the values passed through the current context
-#    - member: name of the memeber
-#    - reason: reason for ban
-#    Outputs:
-#    - response from bot
-# ----------------------------------
-
-@bot.command(name='ban',help="bans a member")
-@has_permissions(administrator=True)
-async def ban(ctx,member:discord.Member,*,reason=None):
-
-    await member.ban(reason=reason)
-    await ctx.send(str(member.mention)+' has been banned for '+reason)
-
-# ----------------------------------
-#    Function: unBan
-#    Description: Command for unbanning 
-#    Inputs:
-#    - ctx: used to access the values passed through the current context
-#    - member: name of the memeber
-#    Outputs:
-#    - response from bot
-# ----------------------------------
-
-@bot.command(name='unban',help="unbans a member")
-@has_permissions(administrator=True)
-async def unban(ctx,*,member):
-
-    banned_users= await ctx.guild.bans()
-
-    member_name,member_discriminator=member.split('#')
-
-    for entry in banned_users:
-
-        user=entry.user
-        if (user.name,user.discriminator)==(member_name,member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send('unbanned '+member.mention)
-            return
 
 # ------------------------------------------------------------------------------------------------------------------
-#    Function: shutdown(ctx)
+#    Function: on_member_join(member)
 #    Description: Command for shutting down the bot
 #    Inputs:
 #    - ctx: used to access the values passed through the current context
